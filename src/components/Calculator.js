fb9557e78b0a9adedf7fddd75c5d4f0f75c1588a
fasaldana <fernando_s_98@hotmail.com>
2022-04-23 18:55:16 -0500

    Added button component to parent

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import calculate from './logic/calculate';
+import ButtonCalc from './buttonCalc';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends Component {
@@ -26,63 +27,25 @@ class Calculator extends Component {
           <input className="number-inp" value={obj.total !== null && obj.next == null ? obj.total : obj.next} readOnly />
         </div>
         <div className="keys">
-          <button name="AC" onClick={this.placeVal} type="button">
-            AC
-          </button>
-          <button name="+/-" onClick={this.placeVal} type="button">
-            +/-
-          </button>
-          <button name="%" onClick={this.placeVal} type="button">
-            %
-          </button>
-          <button name="÷" onClick={this.placeVal} className="orange-btn" type="button">
-            &divide;
-          </button>
-          <button name="7" onClick={this.placeVal} type="button">
-            7
-          </button>
-          <button name="8" onClick={this.placeVal} type="button">
-            8
-          </button>
-          <button name="9" onClick={this.placeVal} type="button">
-            9
-          </button>
-          <button name="x" onClick={this.placeVal} className="orange-btn" type="button">
-            x
-          </button>
-          <button name="4" onClick={this.placeVal} type="button">
-            4
-          </button>
-          <button name="5" onClick={this.placeVal} type="button">
-            5
-          </button>
-          <button name="6" onClick={this.placeVal} type="button">
-            6
-          </button>
-          <button name="-" onClick={this.placeVal} className="orange-btn" type="button">
-            -
-          </button>
-          <button name="1" onClick={this.placeVal} type="button">
-            1
-          </button>
-          <button name="2" onClick={this.placeVal} type="button">
-            2
-          </button>
-          <button name="3" onClick={this.placeVal} type="button">
-            3
-          </button>
-          <button name="+" onClick={this.placeVal} className="orange-btn" type="button">
-            +
-          </button>
-          <button name="0" onClick={this.placeVal} className="big-0" type="button">
-            0
-          </button>
-          <button name="." onClick={this.placeVal} type="button">
-            .
-          </button>
-          <button name="=" onClick={this.placeVal} className="orange-btn" type="button">
-            =
-          </button>
+          <ButtonCalc name="AC" func={this.placeVal} value="AC" />
+          <ButtonCalc name="+/-" func={this.placeVal} value="+/-" />
+          <ButtonCalc name="%" func={this.placeVal} value="%" />
+          <ButtonCalc classname="orange-btn" name="÷" func={this.placeVal} value="÷" />
+          <ButtonCalc name="7" func={this.placeVal} value="7" />
+          <ButtonCalc name="8" func={this.placeVal} value="8" />
+          <ButtonCalc name="9" func={this.placeVal} value="9" />
+          <ButtonCalc classname="orange-btn" name="x" func={this.placeVal} value="x" />
+          <ButtonCalc name="4" func={this.placeVal} value="4" />
+          <ButtonCalc name="5" func={this.placeVal} value="5" />
+          <ButtonCalc name="6" func={this.placeVal} value="6" />
+          <ButtonCalc classname="orange-btn" name="-" func={this.placeVal} value="-" />
+          <ButtonCalc name="1" func={this.placeVal} value="1" />
+          <ButtonCalc name="2" func={this.placeVal} value="2" />
+          <ButtonCalc name="3" func={this.placeVal} value="3" />
+          <ButtonCalc classname="orange-btn" name="+" func={this.placeVal} value="+" />
+          <ButtonCalc classname="big-0" name="0" func={this.placeVal} value="0" />
+          <ButtonCalc name="." func={this.placeVal} value="." />
+          <ButtonCalc classname="orange-btn" name="=" func={this.placeVal} value="=" />
         </div>
       </div>
     );
