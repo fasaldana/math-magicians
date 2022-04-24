f2d4cd694922e8c35b5d80d0d1bf885c11c279fb
fasaldana <fernando_s_98@hotmail.com>
2022-04-23 20:05:52 -0500

    Returned app to class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { Component } from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
-const App = () => {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
+}
 
 export default App;
